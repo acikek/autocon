f431dcca60fb3f01ec6745ca7b698cb2fefea05f
acikek <spiralixp@gmail.com>
2023-05-19 09:26:55 -0700

    init

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+Task Log(LogMessage message) {
+	Console.WriteLine(message.ToString());
+	return Task.CompletedTask;
+}
+
+var client = new DiscordSocketClient();
+var token = Environment.GetEnvironmentVariable("AUTOCON_TOKEN");
+
+client.Log += Log;
+
+await client.LoginAsync(TokenType.Bot, token);
+await client.StartAsync();
+
+await Task.Delay(-1);
